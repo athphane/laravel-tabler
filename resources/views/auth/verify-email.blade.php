4f029cf13c5a85071441f145ab53cb75df17ecc1
Athfan Khaleel <athphane@gmail.com>
2021-01-05 23:29:27 +0500

    - Remove email field from "verify-email" page

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -7,18 +7,20 @@
             <h2 class="card-title text-center mb-4">{{ __('Please verify your email.') }}</h2>
 
             <p class="text-muted">
-                {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we already emailed you? If you didn\'t receive the email, we will gladly send you another. We\'ll send it to the one below.') }}
             </p>
 
-            @if (session('status') == 'verification-link-sent')
+            @if(session('status') == 'verification-link-sent')
                 <p class="text-success">
-                    {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                    {{ __('A new verification link has been sent to your registered email below.') }}
                 </p>
             @endif
 
             <div class="mb-3 mt-4">
-                {!! Form::label('email', __('Email Address'), ['class' => 'form-label'])!!}
-                {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                <label class="form-label">
+                    {{ __('Your Email Address') }}
+                </label>
+                <div class="form-control-plaintext">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="form-footer">
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Tabler';
+    return view('welcome');
 })->name('home');
-
-//Route::get('/dashboard', function () {
-//    return view('tabler');
-//})->middleware(['auth'])->name('dashboard');
-
