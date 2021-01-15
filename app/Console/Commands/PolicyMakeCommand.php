8529175424f18a0d3a2dab2dd0b4adffa772b77a
Athfan Khaleel <athphane@gmail.com>
2021-01-16 01:27:56 +0500

    - Modified PolicyMakeCommand

---
+++ app/Console/Commands/PolicyMakeCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Foundation\Console\PolicyMakeCommand as BasePolicyMakeCommand;
+use Illuminate\Support\Str;
+
+class PolicyMakeCommand extends BasePolicyMakeCommand
+{
+    /**
+     * Replace the model for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $model
+     * @return string
+     */
+    protected function replaceModel($stub, $model)
+    {
+        $model = str_replace('/', '\\', $model);
+
+        if (Str::startsWith($model, '\\')) {
+            $namespacedModel = trim($model, '\\');
+        } else {
+            $namespacedModel = $this->qualifyModel($model);
+        }
+
+        $model = class_basename(trim($model, '\\'));
+
+        $dummyUser = class_basename($this->userProviderModel());
+
+        $dummyModel = Str::camel($model) === 'user' ? 'model' : $model;
+
+        $replace = [
+            'NamespacedDummyModel' => $namespacedModel,
+            '{{ namespacedModel }}' => $namespacedModel,
+            '{{namespacedModel}}' => $namespacedModel,
+            'DummyModel' => $model,
+            '{{ model }}' => $model,
+            '{{model}}' => $model,
+            '{{ pluralModel }}' => pluralize(Str::lower($model), 2),
+            '{{pluralModel}}' => pluralize(Str::lower($model), 2),
+            '{{ singleModel }}' => pluralize(Str::lower($model), 1),
+            '{{singleModel}}' => pluralize(Str::lower($model), 1),
+            'dummyModel' => Str::camel($dummyModel),
+            '{{ modelVariable }}' => Str::camel($dummyModel),
+            '{{modelVariable}}' => Str::camel($dummyModel),
+            'DummyUser' => $dummyUser,
+            '{{ user }}' => $dummyUser,
+            '{{user}}' => $dummyUser,
+            '$user' => '$'.Str::camel($dummyUser),
+        ];
+
+        $stub = str_replace(
+            array_keys($replace), array_values($replace), $stub
+        );
+
+        return str_replace(
+            "use {$namespacedModel};\nuse {$namespacedModel};", "use {$namespacedModel};", $stub
+        );
+    }
+}
